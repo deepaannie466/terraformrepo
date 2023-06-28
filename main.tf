66b2bad9714598313c0d816f698c1c2a0c294ee8
deepaannie466 <51862101+deepaannie466@users.noreply.github.com>
2023-06-28 12:26:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ resource "aws_s3_bucket" "bucket1" {
 	bucket = "vishnu-first-bucket-19oct"
 }
 
-resource "aws_s3_bucket_acl" "example_bucket_acl" {
-    bucket = aws_s3_bucket.bucket1.id
-    acl = "private"
-}
 
 provider "aws" {
     access_key=var.accesskey
